eea43e7cf42690916b74f75b42dde4033f2fa25b
Darshan <darshaner2019@gmail.com>
2025-03-22 22:03:46 +0530

    latest commit

--- backend/models/form.model.js
+++ backend/models/form.model.js
@@ -1,9 +1,12 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-const FormSchema = new mongoose.Schema({
+const FormSchema = new mongoose.Schema(
+  {
     name: { type: String, required: true }, // Village Name
     dist_name: { type: String, required: true }, // District Name
-    state_name: { type: String, required: true } // State Name
-}, { timestamps: true });
+    state_name: { type: String, required: true }, // State Name
+  },
+  { timestamps: true }
+);
 
-export default mongoose.model('Form', FormSchema);
+export default mongoose.model("Form", FormSchema);
